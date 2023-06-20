e644b32ab3076723ed8d9ed428a8ff46cc1d7dfa
toshadrugdealer <t.reikh@yandex.ru>
2023-06-20 15:25:25 +0300

    добавление товара в корзину(первые два в верстке)

---
+++ scriptsjs/cartgeneration.js
@@ -0,0 +1,30 @@
+export const cartGeneration = window.addEventListener(
+  "click",
+  function (event) {
+    if (event.target.hasAttribute("data-cart")) {
+      const cardPr = event.target.closest(".cardProductId");
+      const prodInfo = {
+        id: cardPr.dataset.id,
+        imgSrc: cardPr.querySelector(".cardProdImg").getAttribute("src"),
+        title: cardPr.querySelector(".nameProduct").innerText,
+        price: cardPr.querySelector(".prodPrice").innerText,
+      };
+      const cartProduct = document.querySelector(".product");
+      const cartItemHTML =
+        `<div class="modalCartGeneration" data-id="${prodInfo.id}">
+      <img class="modalCartGenerationImg" src="${prodInfo.imgSrc}" alt="" />
+      <div class="prodPriceBtn">
+        <p class="nameProduct">${prodInfo.title}</p>
+        <p class="prodPrice">${prodInfo.price}</p>
+        <button data-cart type="button" class="btnPushCart">
+          +
+        </button>
+        <button data-cart type="button" class="btnPushCart">
+          -
+        </button>
+      </div>
+    </div>` + cartProduct.innerHTML;
+      cartProduct.innerHTML = cartItemHTML;
+    }
+  }
+);
